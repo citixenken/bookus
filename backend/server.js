83dc8adcc8f924368ff726147ab164b3d68fd1ed
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2023-05-05 16:07:43 +0300

    feat: backend implementation using nodejs/MongoDB

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose"); // ODM library
+const logger = require("morgan");
+const cors = require("cors");
+
+const booksRouter = require("./routes/books");
+
+const app = express();
+
+//middleware
+app.use(logger("dev"));
+app.use(express.json());
+app.use(cors());
+
+// routes
+app.use("/books", booksRouter);
+
+// set up mongo db connection
+const mongoDB = process.env.MONGODB_URI;
+mongoose
+  .connect(mongoDB)
+  .then(() => {
+    // listen to requests once connected to the database
+    const port = process.env.PORT || 4000;
+    app.listen(port, () => {
+      console.log(
+        `***Connected to Mongo DB***. Server is listening on port ${port}`
+      );
+    });
+  })
+  .catch((err) => console.log(err));
