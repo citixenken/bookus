fc23bc9627973dbb8d2607dc7929916b4987d434
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2023-05-08 09:17:30 +0300

    feat: registering new user + hashing/salting password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,17 @@ const mongoose = require("mongoose");
 
 // POST register user
 exports.user_register = async (req, res) => {
-  res.json({ msg: "Register a new user" });
+  //   res.json({ msg: "Register a new user" });
+
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.register(email, password);
+    // if successful, return user email and user object created in database
+    res.status(200).json({ email, user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 // POST login user
