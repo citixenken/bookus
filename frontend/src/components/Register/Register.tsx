d3aa471c40f239fb71bc42ee33392cc50d3634d5
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2023-05-08 17:25:14 +0300

    feat: setting up useRegister custom hook

--- frontend/src/components/Register/Register.tsx
+++ frontend/src/components/Register/Register.tsx
@@ -1,26 +1,17 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { UserAuth } from "../../context/AuthContext";
+import useRegister from "../../hooks/useRegister";
 
 const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-
-  const { createUser } = UserAuth();
+  const { register, isLoading, success, error } = useRegister();
 
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
-    setError("");
-    try {
-      await createUser(email, password);
-      navigate("/home");
-    } catch (err) {
-      setError(err.message);
-      console.log(err.message);
-    }
+    e.preventDefault(); //prevent browser refresh
+    await register(email, password);
   };
 
   return (
@@ -37,6 +28,7 @@ const Register = () => {
           name="email"
           placeholder="Your Email"
           onChange={(e) => setEmail(e.target.value)}
+          value={email}
         />
         <div className="relative">
           <input
@@ -45,22 +37,37 @@ const Register = () => {
             name="password"
             placeholder="Your Password"
             onChange={(e) => setPassword(e.target.value)}
+            value={password}
           />
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="16"
             height="16"
             fill="gray"
-            class="bi bi-eye absolute top-1/2 right-2 -translate-y-1/2"
+            className="bi bi-eye absolute top-1/2 right-2 -translate-y-1/2"
             viewBox="0 0 16 16"
           >
             <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
             <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
           </svg>
         </div>
-        <button className="bg-red-500 rounded-md text-white py-2 hover:scale-105 duration-300">
+        {/* disable register button when loading*/}
+        <button
+          disabled={isLoading}
+          className="bg-red-500 rounded-md text-white py-2 hover:scale-105 duration-300"
+        >
           Register
         </button>
+        {error && (
+          <div className="my-4 text-red-600 border border-red-600 bg-red-100 p-2 rounded-md">
+            {error}
+          </div>
+        )}
+        {success && (
+          <div className="my-4 text-emerald-600 border border-emerald-600 bg-emerald-100 p-2 rounded-md">
+            {success}
+          </div>
+        )}
       </form>
 
       <div className="text-md flex justify-around items-center py-4">
---
+++ frontend/src/hooks/useRegister.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { useAuthContext } from "./useAuthContext";
+
+const useRegister = () => {
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const { dispatch } = useAuthContext();
+
+  const register = async (email, password) => {
+    setIsLoading(true);
+    setError(null);
+
+    const response = await fetch("http://localhost:4000/user/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      setIsLoading(false);
+      setError(data.error);
+      setSuccess(null);
+    }
+
+    if (response.ok) {
+      //   save user email + jwtoken to local storage => avoid logging in every time jwt magic
+      localStorage.setItem("user_data", JSON.stringify(data));
+
+      //   update the AuthContext
+      dispatch({ type: "LOGIN_USER", payload: data });
+
+      setIsLoading(false);
+      setSuccess("User registered successfully");
+    }
+  };
+
+  return { register, isLoading, success, error };
+};
+
+export default useRegister;
