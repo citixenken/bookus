2e7863015d0a7fc418cb533f9feb718d625dfadd
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2023-05-08 12:15:01 +0300

    feat: setting up AuthContext

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import * as Sentry from "@sentry/react";
 import { BrowserTracing } from "@sentry/tracing";
 import App from "./App";
+
 import { BrowserRouter as Router } from "react-router-dom";
 
 // process.env.NODE_ENV === "production" &&
